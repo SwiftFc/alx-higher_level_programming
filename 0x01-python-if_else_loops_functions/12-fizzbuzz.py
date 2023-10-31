0b6439f367f8117ef737d01e5467a7729ac71bcd
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-31 16:50:01 -0700

    a function that prints the numbers and replacing text with multiples of 3 and 5

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(0, 101):
+    for num in range(1, 101):
         if num % 5 == 0 and num % 3 == 0:
             print("FizzBuzz ", end="")
         elif num % 3 == 0:
