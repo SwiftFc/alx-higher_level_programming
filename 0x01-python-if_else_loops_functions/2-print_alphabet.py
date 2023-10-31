a561c6ef46b01730708ad38a8d584db85b1e5419
SwiftFc <aseiduhenrydade@gmail.com>
2023-10-31 13:11:07 -0700

    Solution to task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    convert_to_alpha = chr(letter)
-    print(f"{convert_to_alpha}", end='')
+    print('{:c}'.format(letter), end='')
