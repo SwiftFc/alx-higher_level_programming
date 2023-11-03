051a854aa909f05cbf845ac12f190bd091204d06
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-02 17:52:06 -0700

    task 102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import magic_calculation_102
+
+from magic_calculation_102 import add, sub
+
+def magic_calculation(a, b):
+
+    if a < b:
+        c = add(a, b)
+    else:
+        c = sub(a, b)
+
+    for i in range(4, 6):
+        c = add(c, i)
+
+    return c
