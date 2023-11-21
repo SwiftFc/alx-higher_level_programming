7da2c549f235de9246b621e7cbffa17f0c6d07aa
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-21 05:27:08 -0800

    Initial Commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Represents a square Class"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not an integer
+            VlaueError: if size is less than zero
+        """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
