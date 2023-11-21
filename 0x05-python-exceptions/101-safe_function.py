0934a45672953024cc4f5ac823375e1ceaacc8d1
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-20 17:02:19 -0800

    a function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write(f"Exception: {str(e)}\n")
+        return None
