7459478577768a81e18c4902ed34f164d0cb523e
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-21 05:52:30 -0800

    a class Square that defines a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Represents a class square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
