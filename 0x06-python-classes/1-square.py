06efeed78381882b27a8a5f4437e57149d15c836
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-21 05:06:38 -0800

    a class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""Defines A Class Square"""
+
+
+class Square:
+    """Represent a Square"""
+
+    def __init__(self, size):
+        """Initialize a new Square Attribute.
+
+        Args:
+        size (int): The size of the new square."""
+
+        self.__size = size
