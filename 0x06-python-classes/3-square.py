d04baf9e58b00d95f07eeddb1b0ac59cbfee8271
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-21 05:43:16 -0800

    a class Square that defines a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Defines a Square Class"""
+
+"""Defines a square"""
 
 
 class Square:
-    """Represents a square Class"""
+    """Represents a square"""
 
     def __init__(self, size=0):
         """Initializing this square class
         Args:
             size: represents the size of the square defined
         Raises:
-            TypeError: if size is not an integer
-            VlaueError: if size is less than zero
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
         """
 
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
-    self.__size = size
+        self.__size = size
 
     def area(self):
         """
