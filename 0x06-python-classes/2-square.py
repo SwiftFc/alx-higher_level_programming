847b29a8eb0c362d8bd12012aa81d466e8fea2c1
SwiftFc <aseiduhenrydade@gmail.com>
2023-11-21 05:12:47 -0800

    a class Square that defines a square and handles exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+    """Represents a new sqaure"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+        size (int): The size of the square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
