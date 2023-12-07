5af1b86e35089c8e349899f56837fc21bf0e65b5
SwiftFc <aseiduhenrydade@gmail.com>
2023-12-06 17:50:18 -0800

    changes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
-import random
+
+"""Log Parsing Module"""
+
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+metrics = {
+    'total_size': 0,
+    'status_codes': {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+                     404: 0, 405: 0, 500: 0},
+    'line_count': 0
+}
 
+try:
+    for line in sys.stdin:
+        try:
+            parts = line.split()
+            if len(parts) >= 7:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+                metrics['total_size'] += file_size
+                if status_code in metrics['status_codes']:
+                    metrics['status_codes'][status_code] += 1
+                metrics['line_count'] += 1
+
+                if metrics['line_count'] % 10 == 0:
+                    print("File size: {}".format(metrics['total_size']))
+                    for code in sorted(metrics['status_codes']):
+                        if metrics['status_codes'][code] > 0:
+                            print("{}: {}".format(
+                                code, metrics['status_codes'][code]
+                            ))
+
+        except ValueError:
+            continue
+except KeyboardInterrupt:
+    pass
+
+print("File size: {}".format(metrics['total_size']))
+for code in sorted(metrics['status_codes']):
+    if metrics['status_codes'][code] > 0:
+        print("{}: {}".format(code, metrics['status_codes'][code]))
