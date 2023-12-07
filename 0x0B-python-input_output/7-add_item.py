6b6d5f602a4aeb8eadd7d728b0b8f1d0edcd412e
SwiftFc <aseiduhenrydade@gmail.com>
2023-12-06 17:42:12 -0800

    changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-import sys
 import json
+import sys
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    for arg in sys.argv[1:]:
-        items.append(arg)
+arg_check = len(sys.argv)  # to check number of cmd line args
 
-    save_to_json_file(items, "add_item.json")
+try:
+    # load exixting data from add-item.json
+    my_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    my_list = []  # initialize empty list if it doesn't exist
+
+for x in range(1, arg_check):
+    # append args to my_list
+    my_list.append(sys.argv[x])
+# update my_list with data from add-item.json
+save_to_json_file(my_list, 'add_item.json')
