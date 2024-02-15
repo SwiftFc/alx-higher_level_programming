40cd0548a330a94659c7151b643561bd1716bfd4
SwiftFc <aseiduhenrydade@gmail.com>
2024-02-14 18:27:33 -0800

    mistakes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
-   console.log(languages[i]);
+  console.log(languages[i]);
 }
