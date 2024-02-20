358d8c919d910cc8d6f5eddb63068750d5515137
SwiftFc <aseiduhenrydade@gmail.com>
2024-02-20 06:20:56 -0800

    solutions to tasks

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" python file that contains the class definition of a State and
+an instance Base = declarative_base().
+
+This Module is for the project 0x0F. Python - Object-relational
+mapping proposed by Alx as a test for the implementation
+of sqlalchemy ORM.
+"""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Represents a city for a MySQL database.
+
+    Attributes:
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
