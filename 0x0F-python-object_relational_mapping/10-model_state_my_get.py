3fb0bce9bc1b292a9658733563ffcad40a49362a
SwiftFc <aseiduhenrydade@gmail.com>
2024-02-24 19:08:36 +0000

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,23 +2,37 @@
 """script that prints the State object with the name
 passed as argument from the database hbtn_0e_6_usa
 """
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # establishing a secure connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-        if found is False:
-            print("Not found")
+    # createing a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # executing the cursor to retrieve cities sorted by id
+    city_query = """SELECT cities.id, cities.name, states.name
+                    FROM states
+                    INNER JOIN cities ON states.id = cities.state_id
+                    ORDER BY cities.id ASC"""
+    cursor.execute(city_query)
+
+    # fetching all the results
+    cities = cursor.fetchall()
+
+    # display/print them out
+    for city in cities:
+        print(city)
+
+    # closing the cursor and database connection
+    cursor.close()
+    db.close()
